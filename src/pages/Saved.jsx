1ae831fd6a6303368943c5bd61166619f9e15aa3
abhaykantmishra <155882977+abhaykantmishra@users.noreply.github.com>
2024-07-29 19:16:10 +0530

    Update Saved.jsx

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -7,12 +7,13 @@ const Saved = () => {
 
 
 const [videos , setVideos] = useState([]);
-
+const [pageLoading,setPageLoading] = useState(false);
 
 const getVideo = async (vidIds) => {
   await  axios.post("https://video-app-backend-s7qn.onrender.com/api/v1/video/getvideosbyidarray",{Ids:vidIds})
     .then((res) => {
       setVideos(res.data.videos);
+      setPageLoading(false);
     })
     .catch((err) => {
       console.log(err);
@@ -20,6 +21,7 @@ const getVideo = async (vidIds) => {
 }
   
 const getSavedVideos = async () => {
+  setPageLoading(true);
   await axios.post('https://video-app-backend-s7qn.onrender.com/api/v1/user/getsavedvideos',{
     userId:localStorage.getItem('userId'),
     token:localStorage.getItem('accessToken')
@@ -35,6 +37,33 @@ const getSavedVideos = async () => {
 useEffect(()=>{
   getSavedVideos();
 },[])
+
+  if(videos.length === 0) {
+  return (
+      <>
+     <NoResults text={'No Saved Videos Yet'} />
+      </>
+    )
+}
+
+if(pageLoading === true){
+  return (
+    <>
+    <div className='flex flex-col ml-[40%]'>
+       <Button
+          size="lg"
+          isLoading={pageLoding}
+          loadingText='Loading'
+          colorScheme='white'
+          variant='filled outline'
+          spinnerPlacement='start'
+          className='justify-center items-center text-2xl'
+      >
+      </Button>
+    </div>
+    </>
+  )
+}
 
   return (
     <>
